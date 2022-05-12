36c7b7a4c19b70291be01f7f2fa3fe7418ba0b0a
schoolemmanuel <100888846+schoolemmanuel@users.noreply.github.com>
2022-05-11 20:13:37 -0500

    Primera Version

---
+++ docs/precache-manifest.65c7abfedf5c309eecd0c9fdd02671ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16b64d5b42771d7ee980f1e7a406b2a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57191f652f14178c78c7",
+    "url": "/static/css/main.2d63dc4a.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57191f652f14178c78c7",
+    "url": "/static/js/main.90c1c384.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
